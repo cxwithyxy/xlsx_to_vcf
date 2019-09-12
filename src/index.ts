0d852669c4a0a01cd084c222e97e5687a788f040
chenxuan <523764720@qq.com>
2019-09-12 19:48:16 +0800

    现在知道怎么去读取文件对话框的内容了

--- src/index.ts
+++ src/index.ts
@@ -4,21 +4,24 @@ import { app, BrowserWindow, dialog } from "electron";
 
 app.on("ready", async () =>
 {
-    let bb = new BrowserWindow(
-    {
-        width: 300,
-        webPreferences:
-        {
-            preload: `${__dirname}/../dist/src_browser/Main.js`
-        }
-    })
-    await bb.loadFile("index.html")
-    await bb.webContents.executeJavaScript(`new Main ()`)
-    // console.log(dialog.showOpenDialogSync({ properties: ['openFile'] }))
+    // let bb = new BrowserWindow(
+    // {
+    //     width: 300,
+    //     webPreferences:
+    //     {
+    //         preload: `${__dirname}/../dist/src_browser/Main.js`
+    //     }
+    // })
+    // await bb.loadFile("index.html")
+    // await bb.webContents.executeJavaScript(`new Main ()`)
+
+
+    console.log(dialog.showSaveDialogSync({filters: [
+        { name: 'vcf file', extensions: ['vcf'] },
+    ]}))
     
     // let a = new Xlsx_to_vcf()
     // await a.read_file("M:/aa.xlsx")
-    // console.log("load finish");
     // await a.save_file("M:/a.vcf")
 
     console.log("END");
