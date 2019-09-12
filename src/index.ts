162d857914a86da37c23b89792eb278cc706e082
chenxuan <523764720@qq.com>
2019-09-12 20:03:38 +0800

    完成了我觉得

--- src/index.ts
+++ src/index.ts
@@ -15,20 +15,26 @@ app.on("ready", async () =>
     await bb.loadFile("index.html")
     await bb.webContents.executeJavaScript(`new Main ()`)
 
-    ipcMain.on("tell_you_the_file_path", (e:any, msg:string) =>
+    ipcMain.on("tell_you_the_file_path", async (e:any, msg:string) =>
     {
-        console.log(msg);
-        
+        let xlsx_path = msg
+        let vcf_path = dialog.showSaveDialogSync({filters: [
+            { name: 'vcf file', extensions: ['vcf'] },
+        ]})
+        if(vcf_path)
+        {
+            let a = new Xlsx_to_vcf()
+            try{
+                await a.read_file(xlsx_path)
+                await a.save_file(vcf_path)
+            }
+            catch(e)
+            {
+                dialog.showMessageBox({type:"error", message:"反正报错了，你确定你放进来的是xlsx文件??????"})
+            }
+            
+        }
     })
-    // console.log(dialog.showSaveDialogSync({filters: [
-    //     { name: 'vcf file', extensions: ['vcf'] },
-    // ]}))
-    
-    // let a = new Xlsx_to_vcf()
-    // await a.read_file("M:/aa.xlsx")
-    // await a.save_file("M:/a.vcf")
-
-    console.log("END");
     
 })
 
