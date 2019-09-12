b82dec4919f3a98b12fd5381f0ace9f2a30159e5
chenxuan <523764720@qq.com>
2019-09-12 18:26:30 +0800

    能够获得文件路径了

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-import Excel from "exceljs";
+// import Excel from "exceljs";
 
-var workbook = new Excel.Workbook();
+// var workbook = new Excel.Workbook();
+
+import { app, BrowserWindow } from "electron";
+
+app.on("ready", async () =>
+{
+    let bb = new BrowserWindow(
+    {
+        width: 300,
+        webPreferences:
+        {
+            preload: `${__dirname}/../dist/src_browser/Main.js`
+        }
+    })
+    await bb.loadFile("index.html")
+    await bb.webContents.executeJavaScript(`new Main ()`)
+})
---
+++ src/src_browser/Main.ts
@@ -0,0 +1,25 @@
+
+class Main
+{
+    constructor()
+    {
+        document.addEventListener('drop', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            let files = (<DataTransfer>e.dataTransfer).files
+            for(let i in files)
+            {
+                console.log('File(s) you dragged here: ', files[i].path)
+            }
+        });
+        document.addEventListener('dragover', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+        });
+
+        console.log("readasdasd");
+        
+    }
+}
+
+(<any>global).Main = Main
